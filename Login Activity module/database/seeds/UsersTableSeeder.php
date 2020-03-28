4ce05c77674b5c1c986c7e79601fffc0973fbadf
Mohammad Faisal <shahfaisalsmart1234@gmail.com>
2020-03-28 15:52:09 +0530

    minor bug fixed

--- Login Activity module/database/seeds/UsersTableSeeder.php
+++ Login Activity module/database/seeds/UsersTableSeeder.php
@@ -13,8 +13,8 @@ class UsersTableSeeder extends Seeder
             'email'          => 'admin@admin.com',
             'password'       => '$2y$10$IvTqC9imgJjhy2KzlHeTUe9O9gP2xvknCw7/PnbVhNDgBJdZSydFK',
             'remember_token' => null,
-            'created_at'     => '2019-04-16 08:40:35',
-            'updated_at'     => '2019-04-16 08:40:35',
+            'created_at'     => '2020-02-02 08:40:35',
+            'updated_at'     => '2020-02-03 08:40:35',
             'deleted_at'     => null,
         ]];
 
