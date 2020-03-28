99e197b75db1679ed52dba9d43aef171fdca8a74
Mohammad Faisal <shahfaisalsmart1234@gmail.com>
2020-03-28 15:54:17 +0530

    minor bug fixed

--- Login Activity module/database/seeds/RolesTableSeeder.php
+++ Login Activity module/database/seeds/RolesTableSeeder.php
@@ -10,15 +10,15 @@ class RolesTableSeeder extends Seeder
         $roles = [[
             'id'         => 1,
             'title'      => 'Admin',
-            'created_at' => '2019-04-16 08:40:08',
-            'updated_at' => '2019-04-16 08:40:08',
+            'created_at' => '2020-02-10 08:40:08',
+            'updated_at' => '2020-02-12 08:40:08',
             'deleted_at' => null,
         ],
             [
                 'id'         => 2,
                 'title'      => 'User',
-                'created_at' => '2019-04-16 08:40:08',
-                'updated_at' => '2019-04-16 08:40:08',
+                'created_at' => '2020-02-18 08:40:08',
+                'updated_at' => '2020-02-18 08:40:08',
                 'deleted_at' => null,
             ]];
 
